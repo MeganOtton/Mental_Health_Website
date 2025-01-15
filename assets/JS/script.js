7a9390f0c26ff003abf710496f3ae308044958ff
Megan Otton <115323221+MeganOtton@users.noreply.github.com>
2025-01-15 16:09:05 +0000

    Hero Image, Hero Section, Cards CSS HTML ADD

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     if (navbar) {
         if (window.scrollY > 50) {
-            navbar.style.backgroundColor = '#8EC6DA';
+            navbar.style.backgroundColor = '#434db0';
         } else {
             navbar.style.backgroundColor = '#707CCE';
         }
