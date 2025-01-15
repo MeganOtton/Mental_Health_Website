23474bc4ff802b1d2ad448b4e5d4f18a7e1f113c
Megan Otton <115323221+MeganOtton@users.noreply.github.com>
2025-01-15 16:56:02 +0000

    Card CSS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     if (navbar) {
         if (window.scrollY > 50) {
-            navbar.style.backgroundColor = '#434db0';
+            navbar.style.backgroundColor = '#434eb0f6';
         } else {
             navbar.style.backgroundColor = '#707CCE';
         }
